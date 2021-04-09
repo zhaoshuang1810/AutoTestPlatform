3f50cbd4d2a4bd3b0911ac83cbda7403c071fc61
zhaoshuang1810 <shuang08220>
2021-04-09 11:06:14 +0800

    1

---
+++ CasesProject/testcase/cases/api/test0330.py
@@ -0,0 +1,97 @@
+import unittest
+
+from testcase.com.apirun import ApiRun
+
+
+class Test0330(unittest.TestCase):
+
+	def test_2473_orderSearch2(self):
+		params = {
+			"query": {
+				"channel": "JYWS",
+				"pageIndex": 1,
+				"pageSize": 10,
+				"status": 15,
+				"phoneNumber":"15510311810"
+			}
+		}
+		resp = ApiRun('orderSearch2').execute(params, user='u263')
+		print(resp)
+
+	def test_2473_refundCreate(self):
+		params = {
+			"body": {"channel": "JYWS", "orderNumber": "20210406103909643SGpY7macF1"}
+		}
+		resp = ApiRun('refundCreate').execute(params, user='u263')
+		print(resp)
+
+	def test_2472_refundRuleDetail(self):
+		params = {
+			"path": {"mainOrderNumber": "20210408205718604lX4oy2zG62"},
+			# "body": {"orderNumbers": ["20210402105934008Q1NtaOdezh"]}
+		}
+		resp = ApiRun('refundRuleDetail').execute(params, user='school')
+		print(resp)
+
+	def test_2473_refundCommit(self):
+		params = {
+			"path": {"id": 45},
+			"body": {
+				"submit": True,
+				"refundReason": "退款原因3444",
+				"firstLevelOperationUserEmail": "zs@sunlands.com",
+				"consultationRecord": "11111111111",
+				"consultationImages": "https://img0.baidu.com/it/u=2308190668,2968374054&fm=11&fmt=auto&gp=0.jpg",
+				"realRefundFee": 1,
+				"alipayAccount": "zs11@sunlands.com",
+				"orderNumbers": []
+			}
+		}
+		# params = {
+		# 	"path": {"id": 24},
+		# 	"body": {
+		# 		"submit": False,
+		# 		"refundReason": "撤销原因"
+		# 	}
+		# }
+		resp = ApiRun('refundCommit').execute(params, user='u263')
+		print(resp)
+
+	def test_2470_refundAgree(self):
+		params = {
+			"path": {"id": 45},
+			"body": {
+				"agree": True,
+				"remark": "圆润"
+			}
+		}
+		resp = ApiRun('refundAgree').execute(params, user='u263')
+		print(resp)
+
+	def test_2469_refundFlowDetail(self):
+		params = {
+			"path": {"id": 21}
+		}
+		resp = ApiRun('refundFlowDetail').execute(params, user='u263')
+		print(resp)
+
+	def test_2468_refundDetail(self):
+		params = {
+			"path": {"id": 31}
+		}
+		resp = ApiRun('refundDetail').execute(params, user='school')
+		print(resp)
+
+	def test_2468_refundDetailInfo(self):
+		params = {
+			"path": {"id": 31}
+		}
+		resp = ApiRun('refundDetailInfo').execute(params, user='u263')
+		print(resp)
+
+	def test_2467_refundSearch(self):
+		params = {
+			"body": {"channel": "JYWS"}
+		}
+		resp = ApiRun('refundSearch').execute(params, user='u263')
+		print(resp)
--- CasesProject/testcase/cases/api/testcom.py
+++ CasesProject/testcase/cases/api/testcom.py
@@ -1,4 +1,5 @@
 import os
+import time
 import unittest
 
 from configure.config import PROFILE_PATH, TestEnv
@@ -14,14 +15,15 @@ class TestCom(unittest.TestCase):
 		}
 		resp = ApiRun('phoneNumberDecode').execute(params, user='school')
 		print(resp)
+		phone = resp['decodePhoneNumber']
 		params = {
-			"query": {"phone": resp['decodePhoneNumber']}
+			"query": {"phone": "15510311810"}
 		}
 		resp = ApiRun('getToken').execute(params)
 		print(resp)
 
 	def test_info_update(self):
-		user= 'yanjiao'
+		user = 'jyws'
 		resp = ApiRun('meInfo').execute(user=user)
 		print(resp)
 		USER_DATA = YamlReader(os.path.join(PROFILE_PATH, "userinfo.yaml")).data
@@ -30,10 +32,178 @@ class TestCom(unittest.TestCase):
 		YamlWriter(os.path.join(PROFILE_PATH, "userinfo.yaml")).write(USER_DATA)
 
 	def test_delete_user(self):
-		user= 'yanjiao'
-		resp = ApiRun('meInfo').execute(user=user)
+		params = {
+			"query": {"userId": 138390}
+		}
+		resp = ApiRun('deleteUserIds').execute(params=params)
 		print(resp)
-		USER_DATA = YamlReader(os.path.join(PROFILE_PATH, "userinfo.yaml")).data
-		USER_DATA[TestEnv][user]["userId"] = resp.get("userId")
-		USER_DATA[TestEnv][user]["studentId"] = resp.get("studentId")
-		YamlWriter(os.path.join(PROFILE_PATH, "userinfo.yaml")).write(USER_DATA)
+
+	def test_payment_success(self):
+		params = {
+			"body": {
+				"resultCode": "SUCCESS",
+				"totalFee": 1,
+				"paymentType": 16
+			},
+			"path": {"orderNumber": "20210408173819502t3rzoCb2Ts"}
+		}
+		resp = ApiRun('paymentNotify').execute(params=params, user="school")
+		print(resp)
+
+	def test_create_commodity_tp(self):
+		user = "school"
+		name = "zs勿动-退费测试-TP退费成功-咖啡分期"
+		params = {
+			"body": {
+				"channel": "JYWS",
+				"name": "{}-教学计划".format(name),
+				"categoryId": 1923,
+				"standardPeopleNumber": 100,
+				"classModeCode": "1001",
+				"supportGiving": "false",
+				"supplierId": "0",
+				"tag": "",
+				"introduction": "简介",
+				"specialtyId": 1574,
+				"coverImage": "https://r-exam-fe.ministudy.com/school/test/JYWS/teaching/1606707941652.png",
+				"teachingPlanImage": "",
+				"salesLabel": {
+					"originalPrice": 1000000,
+					"price": 10000,
+					"costPrice": 100
+				},
+				"agreementRuleType": 1000,
+				"agreementRule": {
+					"agreementRuleType": 1000
+				},
+				"property": {},
+				"teachingPlanSubjects": [
+					{
+						"subjectId": 1325,
+						"name": "初级会计实务",
+						"sortNumber": 0
+					}
+				],
+				"type": 1
+			}
+		}
+		resp = ApiRun('teachingPlanCreate').execute(params=params, user=user)
+		tpId = resp['id']
+		params = {
+			"body": {
+				"channel": "JYWS",
+				"beginDate": time.strftime("%Y-%m-%d %H:%M:%S"),
+				"teachingPlanId": tpId,
+				"name": "{}-学习班".format(name),
+				"assistantId": None
+			}
+		}
+		resp = ApiRun('learningClassCreate').execute(params=params, user=user)
+		params = {
+			"body": {
+				"channel": "JYWS",
+				"name": "{}-商品".format(name),
+				"classificationIds": [2079],
+				"coverImage": "https://r-exam-fe.ministudy.com/school/test/JYWS/teaching/1606707941652.png",
+				"shareImage": "",
+				"commodityImage": "",
+				"supplierId": "0",
+				"agreementRuleType": 1000,
+				"agreementRule": {
+					"agreementRuleType": 1000
+				},
+				"originalPrice": 1000000,
+				"price": 10000,
+				"costPrice": 1,
+				"tag": "",
+				"description": "简介",
+				"property": {},
+				"invoice": False,
+				"changeClassSupport": 0,
+				"imSupport": 0,
+				"instalments": "false",
+				"instalmentsType": "",
+				"showApp": True,
+				"showMp": True,
+				"showPc": True,
+				"type": 1,
+				"resellerType": "youzan"
+			}
+		}
+		resp = ApiRun('commodityCreate').execute(params=params, user=user)
+		commodityId = resp['id']
+		params = {
+			"path": {"id": commodityId},
+			"body": [
+				{"resourceId": tpId, "id": commodityId, "resourceType": "TEACHING_PLAN", "channel": "JYWS"}
+			]
+		}
+		resp = ApiRun('commodityAddItem').execute(params=params, user=user)
+		params = {
+			"path": {"id": commodityId}
+		}
+		resp = ApiRun('commodityPublish').execute(params=params, user="school")
+
+	def test_create_commodity_coupon(self):
+		user = "school"
+		name = "zs勿动-退费测试-定金卷"
+		params = {
+			"body": {
+				"channel": "JYWS",
+				"discountType": 10,  # 10优惠卷 20定金卷
+				"name": "{}-优惠卷".format(name),
+				"fullPrice": 10000,  # 满额
+				"discountPrice": 20000,  # 减额
+				"term": 10,  # 有效期：10：固定20：动态
+				"termTime": None,
+				"startDate": time.strftime("%Y-%m-%d %H:%M:%S"),
+				"endDate": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time() + 60 * 60 * 24 * 30)),
+				"type": 10,  # 10全场卷 15单品卷
+			}
+		}
+		resp = ApiRun('couponCreate').execute(params=params, user=user)
+		couponId = resp['id']
+		params = {
+			"body": {
+				"channel": "JYWS",
+				"name": "{}-商品".format(name),
+				"classificationIds": [2079],
+				"coverImage": "https://r-exam-fe.ministudy.com/school/test/JYWS/teaching/1606707941652.png",
+				"shareImage": "",
+				"commodityImage": "",
+				"supplierId": "0",
+				"agreementRuleType": 1000,
+				"agreementRule": {
+					"agreementRuleType": 1000
+				},
+				"originalPrice": 1000000,
+				"price": 10000,
+				"costPrice": 1,
+				"tag": "",
+				"description": "简介",
+				"property": {},
+				"invoice": False,
+				"changeClassSupport": 0,
+				"imSupport": 0,
+				"instalments": "false",
+				"instalmentsType": "",
+				"showApp": True,
+				"showMp": True,
+				"showPc": True,
+				"type": 1,
+				"resellerType": "youzan"
+			}
+		}
+		resp = ApiRun('commodityCreate').execute(params=params, user=user)
+		commodityId = resp['id']
+		params = {
+			"path": {"id": commodityId},
+			"body": [
+				{"resourceId": couponId, "id": commodityId, "resourceType": "COUPON", "channel": "JYWS"}
+			]
+		}
+		resp = ApiRun('commodityAddItem').execute(params=params, user=user)
+		params = {
+			"path": {"id": commodityId}
+		}
+		resp = ApiRun('commodityPublish').execute(params=params, user="school")
